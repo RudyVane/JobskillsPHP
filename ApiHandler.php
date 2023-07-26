f7864bbdade2a4271c9f311e1b4f815dda64e688
Rudy Vane <87861321+RudyVane@users.noreply.github.com>
2023-07-26 14:32:10 +0200

    Add files via upload
    changed layout and prompts

--- ApiHandler.php
+++ ApiHandler.php
@@ -1,16 +1,166 @@
 <?php
+
 class ApiHandler
 {
-    private function readApiKey()
+    // Function to check if the given text is a URL
+    private function isUrl($text)
+    {
+        // Use a regular expression to check the URL
+        $pattern = '/^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/i';
+
+        // Use preg_match to compare the text with the pattern
+        return preg_match($pattern, $text);
+    }
+
+    public function handleFormSubmission()
+    {
+        // Controleer of het formulier is ingediend
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            // Controleer of de vereiste velden zijn ingevuld
+            if (isset($_POST['job']) && isset($_POST['cv'])) {
+                // Haal de ingediende gegevens op
+                $job = $_POST['job'];
+                $cv = $_POST['cv'];
+
+                // Controleer of $job een URL is
+                if ($this->isUrl($job)) {
+                    // Text processing for job offer and CV
+                    $jobOfferText = $this->getPlainTextFromUrl($job);
+				}else {
+					$jobOfferText = $job;
+				}
+
+                // Define the data for the API call for the job offer
+                $data_job_offer = [
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => 'Extract the required skills and qualifications from the following job offer. Please focus only on the skills and qualifications directly relevant to the role. Omit general terms and avoid including unrelated words like 'phone,' 'budget,' and similar non-skill related terms. Your response should include a concise list of essential skills and qualifications required for the job, as a unordered list, your answer must be in dutch:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $jobOfferText,
+                        ],
+                    ],
+                    'temperature' => 0,
+                    'max_tokens' => 3000,
+                    'model' => 'gpt-3.5-turbo-16k',
+                ];
+
+                // Make the API call for the job offer
+                $response_job_offer = $this->call_openai_api($data_job_offer);
+
+                // Define the data for the API call for the CV
+                $data_cv = [
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => 'Extract the jobskills from the candidate\'s CV (use 1 word) as a unordered list in dutch:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $cv,
+                        ],
+                    ],
+                    'temperature' => 0,
+                    'max_tokens' => 3000,
+                    'model' => 'gpt-3.5-turbo-16k',
+                ];
+
+                // Make the API call for the CV
+                $response_cv = $this->call_openai_api($data_cv);
+
+                // Make an API call to compare job offer and CV responses
+                $data_match = [
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => 'Read this job response:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $response_job_offer['choices'][0]['message']['content'],
+                        ],
+                        [
+                            'role' => 'system',
+                            'content' => 'Read this CV response:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $response_cv['choices'][0]['message']['content'],
+                        ],
+                        [
+                            'role' => 'system',
+                            'content' => 'what skills are in both files, your answer must be in dutch',
+                        ],
+                    ],
+                    'temperature' => 0,
+                    'max_tokens' => 3000,
+                    'model' => 'gpt-3.5-turbo-16k',
+                ];
+
+                $response_match = $this->call_openai_api($data_match);
+				
+				$response_percentage = [
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => 'Read this job response:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $response_job_offer['choices'][0]['message']['content'],
+                        ],
+                        [
+                            'role' => 'system',
+                            'content' => 'Read this CV response:',
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $response_cv['choices'][0]['message']['content'],
+                        ],
+                        [
+                            'role' => 'system',
+                            'content' => 'calculate the percentage of skills that are in both files, your answer must be in dutch and you only return the number, no text',
+                        ],
+                    ],
+                    'temperature' => 0,
+                    'max_tokens' => 3000,
+                    'model' => 'gpt-3.5-turbo-16k',
+                ];
+
+                $response_calc = $this->call_openai_api($response_percentage);
+
+                // Store the data in the session to access it in the results page
+                session_start();
+                $_SESSION['job_offer'] = $response_job_offer['choices'][0]['message']['content'];
+                $_SESSION['cv'] = $response_cv['choices'][0]['message']['content'];
+                $_SESSION['matching_skills'] = $response_match['choices'][0]['message']['content'];
+				$_SESSION['match'] = $response_calc['choices'][0]['message']['content'];
+                // Redirect to the results page to display the API response
+                header("Location: results.php");
+                exit();
+            } else {
+                // Als de vereiste velden niet zijn ingevuld, terugsturen naar het formulier met een foutmelding
+                header("Location: index.php?error=missing_fields");
+                exit();
+            }
+        
+        }
+    }
+
+    // Function to read the API key from the file
+    private function read_api_key()
     {
         $file = "api.txt"; // Replace with the path to your api_key.txt file
         $api_key = trim(file_get_contents($file));
         return $api_key;
     }
 
-    public function callOpenAI_API($data)
+    // Function to make the API call using cURL
+    private function call_openai_api($data)
     {
-        $api_key = $this->readApiKey();
+        $api_key = $this->read_api_key();
         $url = 'https://api.openai.com/v1/chat/completions';
         $headers = [
             'Content-Type: application/json',
@@ -25,9 +175,20 @@ class ApiHandler
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($ch);
+        if ($response === false) {
+            // Display cURL error if any
+            echo 'cURL error: ' . curl_error($ch);
+        }
         curl_close($ch);
 
         return json_decode($response, true);
+    }
+
+    // Function to get plain text from a URL and remove HTML tags
+    private function getPlainTextFromUrl($url)
+    {
+        $html = file_get_contents($url);
+        return strip_tags($html);
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,57 +1,31 @@
+<?php
+
+require_once 'ApiHandler.php'; // Include the ApiHandler class
+
+// Create an instance of ApiHandler
+$apiHandler = new ApiHandler();
+
+// Call the handleFormSubmission method
+$apiHandler->handleFormSubmission();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>Vaardigheden uit vacaturetekst filteren</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
+<h2>Vaardigheden vergelijker</h2>
+<p>Deze tool vergelijkt de ingegeven vacaturetekst met jouw CV en geeft de overeenkomsten weer.</p><br>
+    <form action="" method="post">
+        <p>Vacature URL of tekst:</p>
+		<textarea name="job" rows="7" cols="10" placeholder="voer hier de url of de tekst van de vacature in"></textarea>
+        <p>cv</p>
+        <textarea name="cv" rows="7" cols="10" placeholder="voer hier je cv in"></textarea>
 
-<form action="" method="post">
-    <p>vacaturetekst URL:</p>
-    <input type="url" name="url_vacaturetekst" size="100" placeholder="voer hier de URL van de vacaturetekst in">
-    <p>cv</p>
-    <textarea name="cv" rows="10" cols="50" placeholder="voer hier je cv in"></textarea>
-    <br>
-    <input type="submit" value="Submit">
-</form>
-
-<?php
-require_once 'ApiHandler.php';
-require_once 'TextProcessor.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $url_vacaturetekst = $_POST['url_vacaturetekst'];
-    $cv = $_POST['cv'];
-
-    // Initialize TextProcessor and ApiHandler
-    $textProcessor = new TextProcessor();
-    $apiHandler = new ApiHandler();
-
-    // Get plain text from the URL
-    $vacaturetekst = $textProcessor->getPlainTextFromUrl($url_vacaturetekst);
-
-    // Prepare data for API call
-    $data = $textProcessor->prepareDataForApi($vacaturetekst, $cv);
-
-    // Make the API call
-    $response = $apiHandler->callOpenAI_API($data);
-
-    // Handle API response
-    if (!$response) {
-        echo "Failed to communicate with the API.";
-    } else {
-        if (isset($response['error'])) {
-            echo "API Error: " . $response['error']['message'];
-        } else {
-            // Display the generated output
-            if (isset($response['choices'][0]['message']['content'])) {
-                echo "<h2>Resultaat:</h2>";
-                echo nl2br($response['choices'][0]['message']['content']);
-            } else {
-                echo "Failed to extract skills.";
-            }
-        }
-    }
-}
-?>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
